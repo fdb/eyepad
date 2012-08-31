abb83c67dbb741ec812b854de5ab7d87c47943c4
Frederik De Bleser <frederik@burocrazy.com>
2012-08-31 23:00:36 +0200

    Require core.match.
    I'd like to explore to see if core.match is suitable for pattern
    matching the underlying data structures.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "A visual scratchpad for Clojure."
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
+                           [org.clojure/core.match "0.2.0-alpha11"]
                            [congomongo "0.1.10"]]
             :main eyepad.server)
 
