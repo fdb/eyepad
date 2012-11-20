3ce1a8bca3ff9738835ff175479b4f63312a950f
Frederik De Bleser <frederik@burocrazy.com>
2012-11-20 22:29:40 +0100

    Downgrade core.match to avoid AOT bugs.
    AOT compiling the server throws an IllegalArgumentException.
    Downgrading to alpha9 seems to fix the issue.
    
    This seems related to this bug:
    http://dev.clojure.org/jira/browse/MATCH-63

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "A visual scratchpad for Clojure."
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
-                           [org.clojure/core.match "0.2.0-alpha11"]
+                           [org.clojure/core.match "0.2.0-alpha9"]
                            [congomongo "0.1.10"]]
             :main eyepad.server)
 
