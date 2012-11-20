622b593390cbf71ac25fb996fe938900737c0675
Frederik De Bleser <frederik@burocrazy.com>
2012-11-20 11:09:34 +0100

    Add path calculation logic.

--- src/eyepad/graphics.clj
+++ src/eyepad/graphics.clj
@@ -31,6 +31,40 @@
         d (* my-distance (distance x0 y0 x1 y1))]
     (coordinates x0 y0 a d)))
 
+(defn line-length [x0 y0 x1 y1]
+  "Calculate the length of the line."
+  (let [x (Math/pow (Math/abs (- x0 x1)) 2)
+        y (Math/pow (Math/abs (- y0 y1)) 2)]
+    (Math/sqrt x y)))
+
+(defn line-point [t x0 y0 x1 y1]
+  "Calculate location for position t on the line."
+  [(+ x0 (* t (- x1 x0)))
+   (+ y0 (* t (- y1 y0)))])
+
+(defn curve-length 
+  "Calculate the length of the curve segment by integrating it."
+  ([x0 y0 x1 y1 x2 y2 x3 y3] (curve-length x0 y0 x1 y1 x2 y2 x3 y3 20))
+  ([x0 y0 x1 y1 x2 y2 x3 y3 n]
+    ))
+
+(defn curve-point [t x0 y0 x1 y1 x2 y2 x3 y3]
+  "Calculate location for position t on the curve segment."
+  (let [mint (- 1 t)
+        x01 (+ (* x0 mint) (* x1 t))
+        y01 (+ (* y0 mint) (* y1 t))
+        x12 (+ (* x1 mint) (* x2 t))
+        y12 (+ (* y1 mint) (* y2 t))
+        x23 (+ (* x2 mint) (* x3 t))
+        y23 (+ (* y2 mint) (* y3 t))
+        c1x (+ (* x01 mint) (* x12 t))
+        c1y (+ (* y01 mint) (* y12 t))
+        c2x (+ (* x12 mint) (* x23 t))
+        c2y (+ (* y12 mint) (* y23 t))
+        x (+ (* c1x mint) (* c2x t))
+        y (+ (* c1y mint) (* c2y t))]
+    [x y]))
+
 
 (defn line-angle [[x0 y0 :as pos] angle distance]
   "Draw a line between a point and angle/distance."
